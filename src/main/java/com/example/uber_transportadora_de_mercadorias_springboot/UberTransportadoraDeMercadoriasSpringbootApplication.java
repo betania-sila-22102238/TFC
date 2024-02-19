fad4157a1dfa3f9c0acc28565f3005809061dc4d
betania-sila-22102238 <betaniamariaqueta05@gmail.com>
2024-02-19 13:16:33 +0000

    first commit

---
+++ src/main/java/com/example/uber_transportadora_de_mercadorias_springboot/UberTransportadoraDeMercadoriasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.uber_transportadora_de_mercadorias_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberTransportadoraDeMercadoriasSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberTransportadoraDeMercadoriasSpringbootApplication.class, args);
+    }
+
+}
